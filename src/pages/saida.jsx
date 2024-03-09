0cd2224b3c010af9522f4f7dac4814bbe50683b7
ecpieritz <ecpieritz@gmail.com>
2024-03-09 11:27:29 -0300

    feat: add saida component

---
+++ src/pages/saida.jsx
@@ -0,0 +1,18 @@
+import Header from "@/components/header";
+import Menu from "@/components/menu";
+
+export default function Saida() {
+  return (
+    <>
+      <Header />
+
+      <main
+        className={`flex flex-col items-center justify-between py-12`}
+      >
+
+        <h1>Saída</h1>
+
+      </main>
+    </>
+  );
+}
